ece4a7f1988b60ece54fb628c4d7982a0ca99bf9
Gambo16b <131289582+Gambo16b@users.noreply.github.com>
2023-05-12 18:39:36 +0200

    Add files via upload

---
+++ java2.js
@@ -0,0 +1,51 @@
+//esercizio 1
+
+//let num1 = 10;
+//let num2 = 20;
+//let num3 = 35;
+//let num4 = 50;
+//let num5 = 70;
+//let sum = num1+num2+num3+num4+num5;
+//console.log(sum);
+//let media =sum/5;
+//console.log(media);
+
+//esercizio 3
+
+
+//let cod1 = Number (prompt("Inserisci 1"));
+//let cod2 = Number (prompt("Inserisci 2"));
+//let cod3 = Number (prompt("Inserisci 3"));
+//let cod4 = Number (prompt("Inserisci 4"));
+//let cod5 = Number (prompt("Inserisci 5"));
+ //let sum2 = cod1 + cod2 + cod3 + cod4 + cod5;
+
+ //let med2 = sum2/5;
+ //alert ("la somma è " + sum2);
+ //alert ("la media è " + med2);
+
+//esercizio 3
+
+//let tempBari = 24;
+//let tempNaples = 25;
+//let tempRome = 26;
+//let tempMilan = 29;
+
+//let tempMax = Math.max(tempBari,tempMilan,tempNaples,tempRome);
+//console.log("La temperatura più alta è " + tempMax);
+
+// esercizio 4
+
+//const Oggi = 2023 
+//const AnnoNascita = 1945;
+
+//let età = Oggi - AnnoNascita;
+//let moriraitra = 100 - età;
+//alert (`La tua età è ${età}  Ti restano ${moriraitra}`);
+
+//esercizio 5
+
+//const utente1 = prompt("Inserisci il tuo nome ");
+//const utente2 = prompt("Il tuo nome è ");
+//let check = utente1 === utente2;
+//console.log(check)
